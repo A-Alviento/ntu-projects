30666a89bb1886c92fcb839a7d0e0d907ec9f3cb
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2022-08-19 12:22:09 +0800

    made application and mobilePhone class

---
+++ sc2002-oop/sc2002-code-along/src/application.java
@@ -0,0 +1,13 @@
+
+public class application {
+	
+	// main method -> entry point of the program
+	public static void main(String[] args) {
+		
+		// declares an object instance of mobilePhone class
+		mobilePhone myPhone;
+		// initialise the object myPhone
+		myPhone = new mobilePhone();
+		
+	}
+}
---
+++ sc2002-oop/sc2002-code-along/src/mobilePhone.java
@@ -0,0 +1,28 @@
+
+public class mobilePhone {
+	// private so it cannot be modified by other classes
+	// instance variables for the mobilePhone class
+	private String owner;
+	private String colour;
+	private double screenSize;
+	
+	// methods that mobilePhones can do
+	// should be public so other classes can access and use
+	// the method
+	public void ring() {
+		// prints out ring
+		System.out.println("ring, ring, ring");
+	}
+	
+	// method to send an SMS to a receiver
+	// takes in an input of the intended target and the 
+	// sms message and prints it out
+	public String sendSms(String receiver, String message) {
+		// prints the message
+		System.out.println(message);
+		return("Successfully sent");
+	}
+	
+	
+	
+}
